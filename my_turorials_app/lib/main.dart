551be0c77751e47408a3609830a212aff7189409
Ayush750502 <ayush750502@gmail.com>
2024-02-25 00:00:22 +0530

    Adding route for login page

--- my_turorials_app/lib/main.dart
+++ my_turorials_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './login_form_page_03.dart';
 import 'home_page_01.dart';
 import 'test_page_02.dart';
 
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         "./home": (context) =>
             const MyHomePage(title: "Flutter Demo Home Page"),
         "./test": (context) => const TestActivity(),
+        "./login": (context) => const loginFormPage()
       },
     );
   }
